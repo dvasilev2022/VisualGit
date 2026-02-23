2e2985045e16723f295d50e49507a39c724c1e29
vkachulev2022 <d.vasilev2022@pmg-pernik.com>
2026-02-23 09:34:51 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> array = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+           
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] command = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Reverse")
+                {
+                    array.Reverse();
+                }
+                else if (command[0] == "Distinct")
+                {
+                    array = array.Distinct().ToList();
+                }
+                else if (command[0] == "Replace")
+                {
+                    int index = int.Parse(command[1]);
+                    string value = command[2];
+                    array[index] = value;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
